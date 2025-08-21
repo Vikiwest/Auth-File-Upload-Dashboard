4bbdabf0d056bd235213a9626c86840f563c477e
Olorunda Victory <chidiolorunda@gmail.com>
2025-08-21 20:58:07 +0100

    Auth App Uploaded

--- src/utils/api.env.js
+++ src/utils/api.env.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:3001/customers";
+// export const baseUrl = "http://localhost:3001/customers";
+export const baseUrl = "https://my-json-server.typicode.com/Vikiwest/Auth-File-Upload-Dashboard/customers";
